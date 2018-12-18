951a484092334d9ce2f2858d67e874aa0f85c3c2
KwonSeongmin <kwonsm@icloud.com>
2018-12-18 11:31:57 +0900

    create constructor

--- src/main/java/team/balam/security/jwt/AuthenticationException.java
+++ src/main/java/team/balam/security/jwt/AuthenticationException.java
@@ -3,7 +3,15 @@ package team.balam.security.jwt;
 import io.jsonwebtoken.JwtException;
 
 public class AuthenticationException extends Exception {
-    public AuthenticationException(JwtException e) {
+    AuthenticationException(JwtException e) {
         super(e);
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException() {
+
     }
 }
